b7657f8da231dc8b206b529077561f409af285cb
Gidon Handler <gidon.handler1@hp.com>
2022-05-30 15:50:12 +0300

    progress

--- projects/s3-private-image-mng/src/lib/image-manager/image-manager.component.ts
+++ projects/s3-private-image-mng/src/lib/image-manager/image-manager.component.ts
@@ -10,6 +10,34 @@ import { TriggerDirective } from '../trigger.directive';
 })
 export class ImageManagerComponent  {
 
+  temp = [
+    {
+      id: 1,
+      img: "https://media.exapro.com/product/2020/08/P00820091/c9546fbff07a5670ccd0a15071ea8eb3/462x340/hewlett-packard-hp-indigo-7500-digital-press-p00820091_7.jpg"
+  },
+    {
+      id: 2,
+      img: "https://media.exapro.com/product/2020/08/P00820091/c9546fbff07a5670ccd0a15071ea8eb3/462x340/hewlett-packard-hp-indigo-7500-digital-press-p00820091_7.jpg"
+  },
+    {
+      id: 3,
+      img: "https://media.exapro.com/product/2020/08/P00820091/c9546fbff07a5670ccd0a15071ea8eb3/462x340/hewlett-packard-hp-indigo-7500-digital-press-p00820091_7.jpg"
+  },
+    {
+      id: 4,
+      img: "https://media.exapro.com/product/2020/08/P00820091/c9546fbff07a5670ccd0a15071ea8eb3/462x340/hewlett-packard-hp-indigo-7500-digital-press-p00820091_7.jpg"
+  },
+    {
+      id: 5,
+      img: "https://media.exapro.com/product/2020/08/P00820091/c9546fbff07a5670ccd0a15071ea8eb3/462x340/hewlett-packard-hp-indigo-7500-digital-press-p00820091_7.jpg"
+  },
+    {
+      id: 6,
+      img: "https://media.exapro.com/product/2020/08/P00820091/c9546fbff07a5670ccd0a15071ea8eb3/462x340/hewlett-packard-hp-indigo-7500-digital-press-p00820091_7.jpg"
+  },
+]
+
+  selected: any;
   upload: boolean = false;
   objURL: string = '';
   @ViewChild('thumb') thumbnail: ElementRef<HTMLLabelElement> | any;
@@ -23,6 +51,10 @@ export class ImageManagerComponent  {
      img.src = this.objURL;
      this.thumbnail.nativeElement.after(img);
      
+  }
+
+  test(e: any){
+    console.log(e)
   }
 
 
