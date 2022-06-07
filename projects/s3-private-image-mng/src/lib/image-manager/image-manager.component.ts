22d3ecc663f5ba19dbe10da3f7717de319c55823
Gidon Handler <gidon.handler1@hp.com>
2022-06-07 17:58:39 +0300

    progress

--- projects/s3-private-image-mng/src/lib/image-manager/image-manager.component.ts
+++ projects/s3-private-image-mng/src/lib/image-manager/image-manager.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { TriggerDirective } from '../trigger.directive';
 import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 's3imgs-image-manager',
@@ -14,24 +15,33 @@ export class ImageManagerComponent {
   upload: boolean = false;
   images$ = this.http.get<any>(this.trigger.apiPaths.getImages);
 
-  constructor(public trigger: TriggerDirective, private http: HttpClient) { }
+  constructor(public trigger: TriggerDirective, private http: HttpClient, private cd: ChangeDetectorRef) { }
 
   onSelectFiles(e: any, thumb: HTMLImageElement) {
     thumb.src = URL.createObjectURL(e.target.files[0])
   }
 
   onSelectItem() {
-    this.trigger.s3imgSelected.emit(this.selected)
+    this.trigger.s3imgSelected.emit(this.selected);
+    this.trigger.vc.clear();
   }
 
   onUpload(tag: string, file: any) {
     const fd = new FormData;
     fd.append('tag', tag);
     fd.append('file', file.files[0])
-    this.http.post(this.trigger.apiPaths.upload, fd).subscribe()
+    this.http.post(this.trigger.apiPaths.upload, fd).subscribe({
+     
+      next: () => {
+        this.upload = false;
+        this.cd.markForCheck();
+      }
+    })
   }
 
   deleteImage(id: string) {
-    this.http.delete(this.trigger.apiPaths.delete + '/' + id).subscribe()
+    this.http.delete(this.trigger.apiPaths.delete + '/' + id).subscribe({
+      next: () => alert("Image deleted")
+    })
   }
 }
--- projects/s3-private-image-mng/src/lib/trigger.directive.ts
+++ projects/s3-private-image-mng/src/lib/trigger.directive.ts
@@ -1,7 +1,7 @@
-import { ComponentRef, Directive, EventEmitter, HostListener, Injector, Input, Output, ViewContainerRef, ViewRef } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Injector, Input, Output, ViewContainerRef, ViewRef } from '@angular/core';
 import { ImageManagerComponent } from './image-manager/image-manager.component';
 
-export interface ApiPaths{
+export interface ApiPaths {
   getImages: string,
   upload: string,
   delete: string
@@ -12,24 +12,16 @@ export interface ApiPaths{
 })
 export class TriggerDirective {
 
-  viewRef: ViewRef | undefined;
-  @Input('s3imgsTrigger') apiPaths: ApiPaths = {getImages: '/', upload: '/upload', delete: '/delete'}; 
+  @Input('s3imgsTrigger') apiPaths: ApiPaths = { getImages: '/', upload: '/upload', delete: '/delete' };
   @Output() s3imgSelected = new EventEmitter<number>();
 
   @HostListener('click')
   openModal() {
-
-    if (this.viewRef) {
-      this.vc.insert(this.viewRef)
-    }
-    else {
-      let comp: ComponentRef<ImageManagerComponent> = this.vc.createComponent(ImageManagerComponent, { injector: this.injector });
-      this.viewRef = comp.hostView;
-    }
+    this.vc.createComponent(ImageManagerComponent, { injector: this.injector });
   }
 
   constructor(public vc: ViewContainerRef, private injector: Injector) { }
-  
+
 }
 
 
